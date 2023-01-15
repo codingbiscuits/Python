39723846822854fd64ec7569dc6b4ec4788429b2
codingbiscuits <sjbdh.umkc@gmail.com>
2023-01-15 00:41:30 -0600

    Clean up code in snake_game

--- Intermediate/snake_game/main.py
+++ Intermediate/snake_game/main.py
@@ -5,18 +5,11 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-head = t.Turtle()
-head.shape("square")
-head.color("white")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-body = t.Turtle()
-body.shape("square")
-body.color("white")
-body.setposition(-20, 0)
-
-tail = t.Turtle()
-tail.shape("square")
-tail.color("white")
-tail.setposition(-40, 0)
+for position in starting_positions:
+    new_segment = t.Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
 screen.exitonclick()
