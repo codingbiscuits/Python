045d252b4cf41e163e578ea39e246e0bf594ad03
codingbiscuits <sjbdh.umkc@gmail.com>
2023-01-16 13:00:24 -0600

    Added move functionality to snake_game and moving to OOP

--- Intermediate/snake_game/main.py
+++ Intermediate/snake_game/main.py
@@ -1,15 +1,20 @@
 import turtle as t
+from snake import Snake
+import time
 
 screen = t.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snek = Snake()
 
-for position in starting_positions:
-    new_segment = t.Turtle("square")
-    new_segment.color("white")
-    new_segment.goto(position)
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snek.move()
+
 
 screen.exitonclick()
