33790e120212a61e6ba373fc6871e0f5b6414dc3
codingbiscuits <sjbdh.umkc@gmail.com>
2023-01-23 21:05:41 -0600

    Add file reading/writing for snake_game

--- Intermediate/snake_game/main.py
+++ Intermediate/snake_game/main.py
@@ -4,6 +4,7 @@ from food import Food
 from snake import Snake
 from scoreboard import Scoreboard
 
+
 screen = t.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -20,6 +21,9 @@ screen.onkey(snek.down, "Down")
 screen.onkey(snek.left, "Left")
 screen.onkey(snek.right, "Right")
 
+scoreboard.get_highscore()
+scoreboard.update_score()
+
 game_is_on = True
 while game_is_on:
     screen.update()
@@ -30,17 +34,20 @@ while game_is_on:
     if snek.head.distance(food) < 14:
         food.refresh()
         snek.extend()
+        scoreboard.score += 100
         scoreboard.update_score()
 
     # Detect collision with wall
     if snek.head.xcor() >= 300 or snek.head.xcor() <= -300 or snek.head.ycor() >= 300 or snek.head.ycor() <= -300:
         scoreboard.game_over()
+        scoreboard.update_highscore()
         game_is_on = False
 
     # Detect collision with tail
     for segment in snek.segments[1:]:
         if snek.head.distance(segment) < 10:
             scoreboard.game_over()
+            scoreboard.update_highscore()
             game_is_on = False
 
 screen.exitonclick()
--- Intermediate/snake_game/scoreboard.py
+++ Intermediate/snake_game/scoreboard.py
@@ -4,13 +4,14 @@ import turtle as t
 class Scoreboard(t.Turtle):
     def __init__(self):
         super().__init__()
+        self.highscore = 0
         self.score = 0
         self.penup()
         self.hideturtle()
         self.goto(0, 280)
         self.color("white")
         self.speed("fastest")
-        self.write(f"Score: {self.score}", font=(
+        self.write(f"Score: {self.score} High Score: {self.highscore}", font=(
             "Courier", 20), align="center")
 
     def game_over(self):
@@ -21,6 +22,16 @@ class Scoreboard(t.Turtle):
     def update_score(self):
         # self.undo() this also works instead of self.clear()
         self.clear()
-        self.score += 100
-        self.write(f"Score: {self.score}", font=(
+        self.write(f"Score: {self.score} High Score: {self.highscore}", font=(
             "Courier", 20), align="center")
+
+    def get_highscore(self):
+        with open("highscore.txt") as f:
+            highscore = int(f.read())
+        self.highscore += highscore
+
+    def update_highscore(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        with open("highscore.txt", "w+") as wf:
+            wf.write(str(self.highscore))
